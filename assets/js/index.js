3014d02b9809f54755c742c8fb04b8a3a1ba597d
Msajadpour <msajadpour20@gmail.com>
2021-08-26 01:52:35 +0430

    handle : clean codes - add delete for persons - fix localstorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,79 @@
-
-var persons = []
-var person= {};
-var dataToCheckEmpty = []
+var savedPersons = JSON.parse(localStorage.getItem("personsData"));
+var persons;
+savedPersons !== null ? (persons = savedPersons) : (persons = []);
+var dataToCheckEmpty = [];
 var skillList = [];
 var isRepetetive = false;
-var formsTag = $(".input");
+var personsAge ;
 
-
-// onsubmit form 
-$("#submitPerson").on("click", function () { 
+// onsubmit form
+$("#submitPerson").on("click", function () {
     validation(); //validate form
-    
 });
-function setData() {
-    let fname = $("#fname").val()
-    let lname = $("#lname").val()
-    let skills = skillList
-    let date = $("#date").val()
-    person= {
-        fname, 
-        lname,
-        date,
-        skills,
+//handle skills badge and validation them
+$("#addSkill").on("click", function () {
+    let skill = $("#skill").val().trim();
+    if (skill !== "") {
+        if (skillList.length > 0) {
+            for (let i = 0; i < skillList.length; i++) {
+                if (skillList[i] === skill) {
+                    isRepetetive = true;
+                    isValid(false, "#skill");
+                }
+            }
+            if (isRepetetive) {
+                isRepetetive = false;
+            } else {
+                skillList.push(skill);
+                isValid(true, "#skill");
+            }
+        } else {
+            skillList.push(skill);
+        }
+        isValid(true, "#skill");
+    } else {
+        isValid(false, "#skill");
     }
-    skillsValidation()
-    if ( dataToCheckEmpty.includes("") || skillList.length == 0) {// to check non of inputs is empty and skill list is not empty
-        
+    showSkills();
+    $("#skill").val("");
+});
+$("#showPersons").on("click", function () {
+    document.getElementById("redirect").click();
+});
+$("#date").on("blur" , function(){
+    ageCalculator()
+})
+function setData() {
+    let fname = $("#fname").val();
+    let lname = $("#lname").val();
+    let skills = skillList;
+    let age = personsAge
+    let id = `${persons.length + 1}`
+    person = {
+        fname,
+        lname,
+        age,
+        skills,
+        id
+    };
+    skillsValidation();
+    if (dataToCheckEmpty.includes("") || skillList.length == 0) {
+        // to check non of inputs is empty and skill list is not empty
     } else {
         // data.skills = skillList
-        persons.push(person)
-        localStorage.setItem('personsData' , JSON.stringify(persons))
-        console.log(JSON.parse(localStorage.getItem("personsData")))
-        dataCleaner()
-
+        persons.push(person);
+        localStorage.setItem("personsData", JSON.stringify(persons));
+        dataCleaner();
     }
-
 }
 function validation() {
-     // empty to dbl chek
-     dataToCheckEmpty = []
+    var formsTag = $(".input");
+    // empty to dbl chek
+    dataToCheckEmpty = [];
     for (let i = 0; i < formsTag.length; i++) {
-        dataToCheckEmpty.push(formsTag[i].value)
-         // get value of each input and push to "data" array
-        if (formsTag[i].value == "") {
+        dataToCheckEmpty.push(formsTag[i].value.trim());
+        // get value of each input and push to "data" array
+        if (formsTag[i].value.trim() === "") {
             // to check wich input is empty
             formsTag[i].classList.add("is-invalid");
             formsTag[i].classList.remove("is-valid");
@@ -51,57 +82,29 @@ function validation() {
             formsTag[i].classList.add("is-valid");
         }
     }
-    setData()
+    setData();
 }
-function skillsValidation(status){
-    if(status === "none"){
-        $("#skill").removeClass("is-valid")
-    }else{
-        if(skillList.length == 0 ){ // check skill list is empty or not
-            isValid(false , "#skill")
-        }else{
-            isValid(true , "#skill")
-        }
-    }
-}
-
-//handle skills badge and validation them
-$("#addSkill").on("click", function () { 
-    let skill = $("#skill").val();
-    if (skill !== "") {
-        if (skillList.length > 0) {
-            for (let i = 0; i < skillList.length; i++) {
-                if (skillList[i] === skill) {
-                    isRepetetive = true;
-                    isValid(false , "#skill");
-                }
-            }
-            if (isRepetetive) {
-                isRepetetive = false;
-            } else {
-                skillList.push(skill);
-                isValid(true , "#skill");
-            }
-        } else {
-            skillList.push(skill);
-        }
-        isValid(true ,"#skill");
+function skillsValidation(status) {
+    if (status === "none") {
+        $("#skill").removeClass("is-valid");
     } else {
-        isValid(false ,"#skill");
+        if (skillList.length == 0) {
+            // check skill list is empty or not
+            isValid(false, "#skill");
+        } else {
+            isValid(true, "#skill");
+        }
     }
-    showSkills();
-    $("#skill").val("");
-});
-
-function showSkills() { // show array of skills
+}
+function showSkills() {
+    // show array of skills
     $(".showSkills").html("");
     for (let i = 0; i < skillList.length; i++) {
         let skillNode = `<button type="button" id="${i}" class="btn btn-secondary disabled me-2 col-md-2 col-3">${skillList[i]}</button>`;
         $(".showSkills").append(skillNode);
     }
 }
-
-function isValid(status , id) {
+function isValid(status, id) {
     if (status) {
         $(id).addClass("is-valid");
         $(id).removeClass("is-invalid");
@@ -110,18 +113,18 @@ function isValid(status , id) {
         $(id).addClass("is-invalid");
     }
 }
-
-function dataCleaner (){
-    let node = $(".input")
-    for(let i = 0 ; i < node.length ; i++){
-        node[i].value = ""
-        node[i].classList.remove("is-valid")
+function dataCleaner() {
+    let node = $(".input");
+    for (let i = 0; i < node.length; i++) {
+        node[i].value = "";
+        node[i].classList.remove("is-valid");
     }
-    skillList = []
-    skillsValidation("none")
-    showSkills()
-}        
-$("#showPersons").on('click' , function(){
-    console.log("clicked")
-    document.getElementById("redirect").click()
-})
+    skillList = [];
+    skillsValidation("none");
+    showSkills();
+}
+function ageCalculator() {
+    var date = document.getElementById('date').value;
+    var current = +new Date(date);
+    personsAge = Math.floor(((Date.now() - current) / (31557600000)));
+}
--- assets/js/table.js
+++ assets/js/table.js
@@ -1,13 +1,35 @@
-var persons = JSON.parse(localStorage.getItem("personsData"))
-console.log(persons)
-persons.map((person , index)=>{
-    let personsNode = `  
-    <tr>
-      <th scope="row">${index+1}</th>
-      <th scope="row">${person.fname}</th>
-      <td>${person.lname}</td>
-      <td>${person.date}</td>
-      <td>${person.skills.join(" - ")}</td>
-    </tr> `         
-    $("tbody").append(personsNode)
-})
+var persons = JSON.parse(localStorage.getItem("personsData"));
+function showPersons() {
+    if (persons !== null) {
+        persons.map(person => {
+            let personsNode = `  
+          <tr id="${person.id}" class="deletePerson">
+            <td scope="row">${person.id}</td>
+            <td scope="row">${person.fname}</td>
+            <td>${person.lname}</td>
+            <td> ${person.age} ساله</td>
+            <td>${person.skills.join(" - ")}</td>
+            <td><button class="btn btn-danger">حذف</button></td>
+          </tr> `;
+            $("tbody").append(personsNode);
+        });
+    } 
+}
+showPersons();
+function handleDeletePersons(id) {
+  let clonePersons = [...persons];
+  let filteredPersons = clonePersons.filter(p => p.id != id);
+  localStorage.setItem("personsData", JSON.stringify(filteredPersons));
+  persons = JSON.parse(localStorage.getItem("personsData"));
+  $("tbody").empty();
+  showPersons()
+  watcherClasses()
+}
+function watcherClasses(){
+  $(".deletePerson").on("click", function () {
+      handleDeletePersons(this.id);
+  });
+}
+$(".deletePerson").on("click", function () {
+    handleDeletePersons(this.id);
+});
